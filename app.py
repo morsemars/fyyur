5914f982b1aafa001abf499d6b574d5e306f8b8a
Marcelino Madriaga <madriaga.marcelino.jr@gmail.cpm>
2020-01-30 15:16:59 +0800

    BUGFIX: Add Missing Columns in Venue and Artist
    Problem
    Missing Columns like genres, website and seeking details
    Solution
    * added genres, website, seeking_talent,
      seeking_description in Venue.
    * added website, seeking_venue, seeking_description in Artist
    * added Shows table

--- app.py
+++ app.py
@@ -30,35 +30,45 @@ migrate = Migrate(app, db)
 # Models.
 #----------------------------------------------------------------------------#
 
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+Shows = db.Table('Shows', 
+  db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key=True), 
+  db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True),
+  db.Column('start_time', db.DateTime, nullable = False))
+
 class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
+    name = db.Column(db.String, nullable = False)
+    city = db.Column(db.String(120),  nullable = False)
+    state = db.Column(db.String(120),  nullable = False)
+    address = db.Column(db.String(120),  nullable = False)
+    phone = db.Column(db.String(120),  nullable = False)
+    image_link = db.Column(db.String(500),  nullable = True)
+    facebook_link = db.Column(db.String(120),  nullable = True)
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    genres = db.Column(db.String(120),  nullable = False)
+    website = db.Column(db.String(120),  nullable = True)
+    seeking_talent = db.Column(db.String, nullable = False)
+    seeking_description = db.Column(db.String, nullable = True)
+    artists = db.relationship('Artist', secondary=Shows, backref=db.backref('Venues', lazy=True))
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
+    name = db.Column(db.String,  nullable = False)
+    city = db.Column(db.String(120),  nullable = False)
+    state = db.Column(db.String(120),  nullable = False)
+    phone = db.Column(db.String(120),  nullable = False)
+    genres = db.Column(db.String(120),  nullable = False)
+    image_link = db.Column(db.String(500),  nullable = True)
+    facebook_link = db.Column(db.String(120),  nullable = True)
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+    website = db.Column(db.String(120),  nullable = True)
+    seeking_venue = db.Column(db.String, nullable = False)
+    seeking_description = db.Column(db.String, nullable = True)
 
 #----------------------------------------------------------------------------#
 # Filters.
