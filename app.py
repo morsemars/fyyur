cda7b37db4ce6932135cbefe339cfd222b4eb8c8
Marcelino Madriaga <madriaga.marcelino.jr@gmail.cpm>
2020-01-30 13:52:00 +0800

    NEW: Add Data Migration
    Problem
    No existing data migration capability
    Solution
    * created migration using flask_migrate
    * initialized migration repository structure
    * generated migration script
    * ran generated script

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import json
 import dateutil.parser
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 import logging
@@ -20,6 +21,8 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 # TODO: connect to a local postgresql database
 
